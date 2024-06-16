e6e244106c69e16b5f74be31b1c6b2c96e8c0f80
Larissa Yasmim <yasmimlarissa09@gmail.com>
2024-06-16 17:37:53 -0300

    Organiza a mensagem com os dados do usuário

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -5,16 +5,16 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Por favor, digite o seu nome! (Após pressione Enter)");
+        System.out.println("Por favor, digite o seu nome: (Após pressione Enter)");
         String nomeCliente = scanner.next();
 
-        System.out.println("Por favor, digite o número da sua conta! (Após pressione Enter)");
+        System.out.println("Por favor, digite o número da sua conta: (Após pressione Enter)");
         int numero = scanner.nextInt();
         
-        System.out.println("Por favor, digite o número da Agência! (Após pressione Enter)");
+        System.out.println("Por favor, digite o número da Agência: (Após pressione Enter)");
         String agencia = scanner.next();
         
-        System.out.println("Por favor, digite o seu saldo! (Após pressione Enter)");
+        System.out.println("Por favor, digite o seu saldo: (Após pressione Enter)");
         double saldo = scanner.nextDouble() ; 
 
         System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia+ ",");
