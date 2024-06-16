804a81dd065de79bf27aa56f1706d20514972f02
Larissa Yasmim <yasmimlarissa09@gmail.com>
2024-06-16 16:29:15 -0300

    Adiciona mensagens de solicitação ao usuário

---
+++ ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome! (Após pressione Enter)");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua conta! (Após pressione Enter)");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da Agência! (Após pressione Enter)");
+        String agencia = scanner.next();
+        
+        System.out.println("Por favor, digite o seu saldo! (Após pressione Enter)");
+        double saldo = scanner.nextDouble() ; 
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ",\n conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
